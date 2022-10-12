f8bef394c9bae1dc2c917a3418d37f799e5baae7
Yael Balbastre <yael.balbastre@gmail.com>
2022-10-12 19:07:06 -0400

    FIX(init): oops typo

--- jitfields/__init__.py
+++ jitfields/__init__.py
@@ -1,4 +1,4 @@
-from distance import euclidean_distance_transform, l1_distance_transform
-from pushpull import pull, push, count, grad
-from resize import resize, restrict
-from splinc import spline_coeff, spline_coeff_, spline_coeff_nd, spline_coeff_nd_
+from .distance import euclidean_distance_transform, l1_distance_transform
+from .pushpull import pull, push, count, grad
+from .resize import resize, restrict
+from .splinc import spline_coeff, spline_coeff_, spline_coeff_nd, spline_coeff_nd_
