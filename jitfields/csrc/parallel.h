02514526b5b774bde01ea3d6f81729632dc05b6e
Yael Balbastre <yael.balbastre@gmail.com>
2023-01-05 10:51:36 -0500

    FIX(parallel): only use if llvm recent enough

--- jitfields/csrc/parallel.h
+++ jitfields/csrc/parallel.h
@@ -1,7 +1,31 @@
 #ifndef JF_PARALLEL_H
 #define JF_PARALLEL_H
-#include "threadpool.h"
-#include "parallel_impl.h"
+
+/* While <future> (and therefore our thread pool) works fine on MacOS,
+ * it fails on Linux, apparently because of the older LLVM under the hood.
+ * See:
+ *      https://github.com/wlav/cppyy/issues/60
+ *      https://stackoverflow.com/questions/73424050
+ * For now I am checking the llvm (clang) version and only enable the
+ * thread pool if is is recent enough. My mac laptop has version 14, and
+ * one of the issues above mentions version 13, so I enable it for
+ * versions >= 13.
+ *
+ * Note that I had to disable the use of thread_local in parallel_impl.h,
+ * which fail even on MacOS.
+ */
+#define JF_CAN_USE_PARALLEL 1
+#if __clang__
+#if __clang_major__ < 13
+#undef  JF_CAN_USE_PARALLEL
+#define JF_CAN_USE_PARALLEL 0
+#endif
+#endif
+
+#if JF_CAN_USE_PARALLEL
+#  include "threadpool.h"
+#  include "parallel_impl.h"  // cannot import until cppyy is fixed
+#endif
 
 /* LICENSE:
  * Most of the functions are adapted from PyTorch/ATen's ParallelNative
@@ -17,6 +41,10 @@ inline void parallel_for(long begin, long end, long grain_size, const F& f)
 {
     if (begin >= end) return;
 
+#if !(JF_CAN_USE_PARALLEL)
+    f(begin, end);
+    return;
+#else
     const auto numiter = end - begin;
     const bool use_parallel =  (numiter > grain_size && numiter > 1 &&
                                 // !internal::in_parallel_region() &&
@@ -28,6 +56,7 @@ inline void parallel_for(long begin, long end, long grain_size, const F& f)
     }
 
     internal::invoke_parallel(begin, end, grain_size, f);
+#endif
 }
 
 } // namespace jf
