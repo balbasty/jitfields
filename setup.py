3672d1c4f7fe0288541137e395f9bea7ae0432ce
Yael Balbastre <ybalbastre@mgh.harvard.edu>
2023-04-02 20:45:54 -0400

    MNT(setup): FIX -- include package data using both setup.py *and* MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
 from setuptools import setup, find_packages
 
-setup(packages=find_packages())
+setup(
+    packages=find_packages(),
+    package_data={'jitfields': ['csrc/**/*']},
+)
