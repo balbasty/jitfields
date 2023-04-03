2698f48ce78d2855b3436b1e89cfbd017d9b7a48
Yael Balbastre <ybalbastre@mgh.harvard.edu>
2023-04-02 20:33:59 -0400

    MNT(setup): FIX -- add sources as package data

--- setup.py
+++ setup.py
@@ -1,10 +1,4 @@
 #!/usr/bin/env python3
 from setuptools import setup, find_packages
 
-setup(
-    packages=find_packages(),
-    package_data={'diffeo': [
-        'diffeo/csrc/*',
-        'diffeo/csrc/**/*',
-    ]},
-)
+setup(packages=find_packages())
