08d81e72df74cc50e29a0240bbaaf132629be56b
Yael Balbastre <yael.balbastre@gmail.com>
2023-05-08 18:36:12 -0400

    API(pushpull): expose backward passses (undocumented for now)

--- jitfields/pushpull_backward.py
+++ jitfields/pushpull_backward.py
@@ -1,67 +1,47 @@
 """
-## Overview
-This module contains a function (`pull`) for sampling values at arbitrary
-coordinates into a continuous function encoded by regularly spaced splines.
-
-The numerical adjoint of this function (`push`) is also defined. The
-operation performed by the adjoint is commonly known as "splatting" in
-computer visions, since it associates each input value with a coordinate
-into the output volume.
-
-The `count` function is equivalent to applying `push` to an image of
-`ones`, but uses a faster implementation.
-
-Finally, `grad` is related to `pull`, as it allows to sample the directional
-gradients of the continuous function at arbitrary locations.
-
-## Notes
-
-In all cases, the last dimension of `grid` assumes a "left to right" order
-of indices, such that if the shape of `inp` is `(Nx, Ny, Nz, Nc)`, each
-element in  `grid` contains the indices `(x, y, z)`.
-This differs from PyTorch, where indices `(x, y, z)` index into a tensor
-ordered as `(Nb, Nc, Nz, Ny, Nx)`.
-
-Furthermore, indices are expressed in voxels, and are therefore expected
-in the range `(0, N-1)`. Indices outside of this range are dealt with using
-one of the boundary conditions that are implemented. This again differs
-form PyTorch, where indices are in the normalized range `(-1, 1)`.
+Semi-hidden module that exposes the (non-differentiable) backward passes
+of push/pull.
 """
-__all__ = ['pull', 'push', 'count', 'grad']
+__all__ = ['pull_backward', 'push_backward', 'count_backward', 'grad_backward']
 
 import torch
 from torch import Tensor
-from typing import Optional, Sequence
+from typing import Tuple
 from .utils import try_import, ensure_list
 from .typing import OneOrSeveral, BoundType, OrderType, ExtrapolateType
-from .splinc import spline_coeff_nd, spline_coeff_nd_
+from .splinc import spline_coeff_nd
 from .bindings.common.bounds import convert_bound
 from .bindings.common.spline import convert_order
 cuda_pushpull = try_import('jitfields.bindings.cuda', 'pushpull')
 cpu_pushpull = try_import('jitfields.bindings.cpp', 'pushpull')
 
 
-def pull(
+def pull_backward(
+    grad: Tensor,
     inp: Tensor,
     grid: Tensor,
     order: OneOrSeveral[OrderType] = 2,
     bound: OneOrSeveral[BoundType] = 'dct2',
     extrapolate: ExtrapolateType = True,
     prefilter: bool = False,
-    out: Optional[Tensor] = None,
-) -> Tensor:
+) -> Tuple[Tensor, Tensor]:
     """Sample a tensor using spline interpolation
 
     Notes
     -----
-    By default, `inp` is assumed to contain the coefficients of a
+    - By default, `inp` is assumed to contain the coefficients of a
     continuous function encoded by regularly spaced cubic splines.
     Instead, when `prefilter` is `True`, `pull` interpolates the values
     of `inp`. To this end, `inp` is first converted to spline coefficients
     (_i.e._, prefiltered).
 
+    - Backpropagation does not work through this function!
+
     Parameters
     ----------
+    grad : `(..., *outshape, channel) tensor`
+        Gradient with respect to the output of `pull`,
+        with shape `(..., *outshape, channel)`.
     inp : `(..., *inshape, channel) tensor`
         Input tensor with shape `(..., *inshape, channel)`.
     grid : `(..., *outshape, ndim) tensor`
@@ -83,8 +63,12 @@ def pull(
 
     Returns
     -------
-    out : `(..., *outshape, channel) tensor`
-        Pulled tensor, with shape `(..., *outshape, channel)`.
+    grad_inp : `(..., *inshape, channel) tensor`
+        Gradient with respect to the input tensor,
+         with shape `(..., *inshape, channel)`.
+    grad_grid : `(..., *outshape, ndim) tensor`
+        Gradient with respect to the tensor of coordinates into `inp`,
+        with shape `(..., *outshape, ndim)`.
 
     """
     ndim = grid.shape[-1]
@@ -94,30 +78,40 @@ def pull(
         inp = spline_coeff_nd(inp.movedim(-1, 0), order, bound, ndim).movedim(0, -1)
     inp, grid = _broadcast_pull(inp, grid)
     order, bound, extrapolate = _preproc_opt(order, bound, extrapolate, ndim)
-    return Pull.apply(inp, grid, order, bound, extrapolate, out)
+
+    bwd = (cuda_pushpull if grad.is_cuda else cpu_pushpull).pull_backward
+    outgrad_inp = torch.zeros_like(inp)
+    outgrad_grid = torch.empty_like(grid)
+    bwd(outgrad_inp, outgrad_grid, grad, inp, grid, order, bound, extrapolate)
+    return outgrad_inp, outgrad_grid
 
 
-def grad(
+def grad_backward(
+    grad: Tensor,
     inp: Tensor,
     grid: Tensor,
     order: OneOrSeveral[OrderType] = 2,
     bound: OneOrSeveral[BoundType] = 'dct2',
     extrapolate: ExtrapolateType = True,
     prefilter: bool = False,
-    out: Optional[Tensor] = None,
-) -> Tensor:
+) -> Tuple[Tensor, Tensor]:
     """Sample the spatial gradients of a tensor using spline interpolation
 
     Notes
     -----
-    By default, `inp` is assumed to contain the coefficients of a
+    - By default, `inp` is assumed to contain the coefficients of a
     continuous function encoded by regularly spaced cubic splines.
     Instead, when `prefilter` is `True`, `grad` interpolates the values
     of `inp`. To this end, `inp` is first converted to spline coefficients
     (_i.e._, prefiltered).
 
+    - Backpropagation does not work through this function!
+
     Parameters
     ----------
+    grad : `(..., *outshape, channel, ndim) tensor`
+        Gradient with respect to the output of `grad`,
+        with shape `(..., *outshape, channel, ndim)`.
     inp : `(..., *inshape, channel) tensor`
         Input tensor, with shape `(..., *inshape, channel)`.
     grid : `(..., *outshape, ndim) tensor`
@@ -139,8 +133,12 @@ def grad(
 
     Returns
     -------
-    out : `(..., *outshape, channel, ndim) tensor`
-        Pulled gradients, with shape `(..., *outshape, channel, ndim)`.
+    grad_inp : `(..., *inshape, channel) tensor`
+        Gradient with respect to the input tensor,
+         with shape `(..., *inshape, channel)`.
+    grad_grid : `(..., *outshape, ndim) tensor`
+        Gradient with respect to the tensor of coordinates into `inp`,
+        with shape `(..., *outshape, ndim)`.
 
     """
     ndim = grid.shape[-1]
@@ -150,29 +148,38 @@ def grad(
         inp = spline_coeff_nd(inp.movedim(-1, 0), order, bound, ndim).movedim(0, -1)
     inp, grid = _broadcast_pull(inp, grid)
     order, bound, extrapolate = _preproc_opt(order, bound, extrapolate, ndim)
-    return Grad.apply(inp, grid, order, bound, extrapolate, out)
+
+    bwd = (cuda_pushpull if grad.is_cuda else cpu_pushpull).grad_backward
+    outgrad_inp = torch.zeros_like(inp)
+    outgrad_grid = torch.empty_like(grid)
+    bwd(outgrad_inp, outgrad_grid, grad, inp, grid, order, bound, extrapolate)
+    return outgrad_inp, outgrad_grid
 
 
-def push(
+def push_backward(
+    grad: Tensor,
     inp: Tensor,
     grid: Tensor,
-    shape: Optional[Sequence[int]] = None,
     order: OneOrSeveral[OrderType] = 2,
     bound: OneOrSeveral[BoundType] = 'dct2',
     extrapolate: ExtrapolateType = True,
     prefilter: bool = False,
-    out: Optional[Tensor] = None,
-) -> Tensor:
+) -> Tuple[Tensor, Tensor]:
     """Splat a tensor using spline interpolation
+
+    Notes
+    -----
+    - Backpropagation does not work through this function!
 
     Parameters
     ----------
+    grad : `(..., *outshape, channel) tensor`
+        Gradient with respect to the output of `push`,
+        with shape `(..., *outshape, channel)`.
     inp : `(..., *inshape, channel) tensor`
         Input tensor, with shape `(..., *inshape, channel)`.
     grid : `(..., *inshape, ndim) tensor`
         Tensor of coordinates into `inp`, with shape `(..., *inshape, ndim)`.
-    shape : `sequence[int]`, default=`inshape`
-        Output spatial shape. Default is `inshape`.
     order : `[sequence of] {0..7}`, default=2
         Interpolation order (per dimension).
     bound : `[sequence of] {'zero', 'replicate', 'dct1', 'dct2', 'dst1', 'dst2', 'dft'}`, default='dct2'
@@ -190,38 +197,52 @@ def push(
 
     Returns
     -------
-    out : `(..., *shape, channel) tensor`
-        Pushed tensor, with shape `(..., *shape, channel)`.
+    grad_inp : `(..., *inshape, channel) tensor`
+        Gradient with respect to the input tensor,
+         with shape `(..., *inshape, channel)`.
+    grad_grid : `(..., *inshape, ndim) tensor`
+        Gradient with respect to the tensor of coordinates into `inp`,
+        with shape `(..., *inshape, ndim)`.
 
     """
     ndim = grid.shape[-1]
     if ndim > 3:
         raise NotImplementedError("Not implemented for spatial dim > 3")
     inp, grid = _broadcast_push(inp, grid)
-    shape = list(shape or inp.shape[-ndim-1:-1])
     order, bound, extrapolate = _preproc_opt(order, bound, extrapolate, ndim)
-    inp = Push.apply(inp, grid, shape, order, bound, extrapolate, out)
+
     if prefilter:
-        inp = spline_coeff_nd_(inp.movedim(-1, 0), order, bound, ndim).movedim(0, -1)
-    return inp
+        grad = grad.movedim(-1, 0)
+        grad = spline_coeff_nd(grad, order, bound, ndim)
+        grad = grad.movedim(0, -1)
+
+    bwd = (cuda_pushpull if grad.is_cuda else cpu_pushpull).push_backward
+    outgrad_inp = torch.empty_like(inp)
+    outgrad_grid = torch.empty_like(grid)
+    bwd(outgrad_inp, outgrad_grid, grad, inp, grid, order, bound, extrapolate)
+    return outgrad_inp, outgrad_grid
 
 
-def count(
+def count_backward(
+    grad: Tensor,
     grid: Tensor,
-    shape: Optional[Sequence[int]] = None,
     order: OneOrSeveral[OrderType] = 2,
     bound: OneOrSeveral[BoundType] = 'dct2',
     extrapolate: ExtrapolateType = True,
-    out: Optional[Tensor] = None,
 ) -> Tensor:
     """Splat ones using spline interpolation
 
+    Notes
+    -----
+    - Backpropagation does not work through this function!
+
     Parameters
     ----------
+    grad : `(..., *outshape) tensor`
+        Gradient with respect to the output of `count`,
+        with shape `(..., *outshape, channel)`.
     grid : `(..., *inshape, ndim) tensor`
         Tensor of coordinates, with shape `(..., *inshape, ndim)`
-    shape : `sequence[int]`, default=`inshape`
-        Output spatial shape. Default is `inshape`.
     order : `[sequence of] {0..7}`, default=2
         Interpolation order (per dimension).
     bound : `[sequence of] {'zero', 'replicate', 'dct1', 'dct2', 'dst1', 'dst2', 'dft'}`, default='dct2'
@@ -235,105 +256,20 @@ def count(
 
     Returns
     -------
-    out : `(..., *shape) tensor`
-        Pushed ones, with shape `(..., *shape)`.
+    grad_grid : `(..., *inshape, ndim) tensor`
+        Gradient with respect to the tensor of coordinates,
+        with shape `(..., *inshape, ndim)`.
 
     """
     ndim = grid.shape[-1]
     if ndim > 3:
         raise NotImplementedError("Not implemented for spatial dim > 3")
-    shape = list(shape or grid.shape[-ndim-1:-1])
     order, bound, extrapolate = _preproc_opt(order, bound, extrapolate, ndim)
-    return Count.apply(grid, shape, order, bound, extrapolate, out)
 
-
-class Pull(torch.autograd.Function):
-
-    @staticmethod
-    def forward(ctx, inp, grid, order, bound, extrapolate, out):
-        fwd = (cuda_pushpull if inp.is_cuda else cpu_pushpull).pull
-        ctx.opt = (order, bound, extrapolate)
-        ctx.save_for_backward(inp, grid)
-        fullshape = grid.shape[:-1] + inp.shape[-1:]
-        out = inp.new_empty(fullshape) if out is None else out.view(fullshape)
-        out = fwd(out, inp, grid, order, bound, extrapolate)
-        return out
-
-    @staticmethod
-    def backward(ctx, grad):
-        bwd = (cuda_pushpull if grad.is_cuda else cpu_pushpull).pull_backward
-        inp, grid = ctx.saved_tensors
-        outgrad_inp = torch.zeros_like(inp)
-        outgrad_grid = torch.empty_like(grid)
-        bwd(outgrad_inp, outgrad_grid, grad, inp, grid, *ctx.opt)
-        return (outgrad_inp, outgrad_grid) + (None,) * 4
-
-
-class Push(torch.autograd.Function):
-
-    @staticmethod
-    def forward(ctx, inp, grid, shape, order, bound, extrapolate, out):
-        fwd = (cuda_pushpull if inp.is_cuda else cpu_pushpull).push
-        ctx.opt = (order, bound, extrapolate)
-        ctx.save_for_backward(inp, grid)
-        ndim = grid.shape[-1]
-        fullshape = list(grid.shape[:-ndim-1]) + list(shape) + list(inp.shape[-1:])
-        out = inp.new_zeros(fullshape) if out is None else out.view(fullshape)
-        out = fwd(out, inp, grid, order, bound, extrapolate)
-        return out
-
-    @staticmethod
-    def backward(ctx, grad):
-        bwd = (cuda_pushpull if grad.is_cuda else cpu_pushpull).push_backward
-        inp, grid = ctx.saved_tensors
-        outgrad_inp = torch.empty_like(inp)
-        outgrad_grid = torch.empty_like(grid)
-        bwd(outgrad_inp, outgrad_grid, grad, inp, grid, *ctx.opt)
-        return (outgrad_inp, outgrad_grid) + (None,) * 5
-
-
-class Count(torch.autograd.Function):
-
-    @staticmethod
-    def forward(ctx, grid, shape, order, bound, extrapolate, out):
-        fwd = (cuda_pushpull if grid.is_cuda else cpu_pushpull).count
-        ctx.opt = (order, bound, extrapolate)
-        ctx.save_for_backward(grid)
-        ndim = grid.shape[-1]
-        fullshape = list(grid.shape[:-ndim-1]) + list(shape) + [1]
-        out = grid.new_zeros(fullshape) if out is None else out.view(fullshape)
-        out = fwd(out, grid, order, bound, extrapolate).squeeze(-1)
-        return out
-
-    @staticmethod
-    def backward(ctx, grad):
-        bwd = (cuda_pushpull if grad.is_cuda else cpu_pushpull).count_backward
-        grid, = ctx.saved_tensors
-        outgrad_grid = torch.empty_like(grid)
-        bwd(outgrad_grid, grad.unsqueeze(-1), grid, *ctx.opt)
-        return (outgrad_grid,) + (None,) * 5
-
-
-class Grad(torch.autograd.Function):
-
-    @staticmethod
-    def forward(ctx, inp, grid, order, bound, extrapolate, out):
-        fwd = (cuda_pushpull if inp.is_cuda else cpu_pushpull).grad
-        ctx.opt = (order, bound, extrapolate)
-        ctx.save_for_backward(inp, grid)
-        fullshape = grid.shape[:-1] + inp.shape[-1:] + grid.shape[-1:]
-        out = inp.new_empty(fullshape) if out is None else out.view(fullshape)
-        out = fwd(out, inp, grid, order, bound, extrapolate)
-        return out
-
-    @staticmethod
-    def backward(ctx, grad):
-        bwd = (cuda_pushpull if grad.is_cuda else cpu_pushpull).grad_backward
-        inp, grid = ctx.saved_tensors
-        outgrad_inp = torch.zeros_like(inp)
-        outgrad_grid = torch.empty_like(grid)
-        bwd(outgrad_inp, outgrad_grid, grad, inp, grid, *ctx.opt)
-        return (outgrad_inp, outgrad_grid) + (None,) * 4
+    bwd = (cuda_pushpull if grad.is_cuda else cpu_pushpull).count_backward
+    outgrad_grid = torch.empty_like(grid)
+    bwd(outgrad_grid, grad.unsqueeze(-1), grid, order, bound, extrapolate)
+    return outgrad_grid
 
 
 def _preproc_opt(order, bound, extrapolate, ndim):
