2ceeee9c9dd027d7ba3359f9cc9e62473091d5dd
Yael Balbastre <ybalbastre@mgh.harvard.edu>
2024-01-17 13:25:27 -0500

    DOC(bounds): list (and add) aliases

--- jitfields/typing.py
+++ jitfields/typing.py
@@ -2,8 +2,24 @@ from typing import TypeVar, Union, Sequence, Optional
 
 try:
     from typing import Literal
+    LiteralInt = LiteralStr = LiteralStrOpt = Literal
 except ImportError:
-    Literal = None
+
+    class _LiteralInt:
+        def __getitem__(self, _):
+            return int
+
+    class _LiteralStr:
+        def __getitem__(self, _):
+            return str
+
+    class _LiteralStrOpt:
+        def __getitem__(self, _):
+            return Optional[str]
+
+    LiteralInt = _LiteralInt()
+    LiteralStr = _LiteralStr()
+    LiteralStrOpt = _LiteralStrOpt()
 
 
 T = TypeVar('T')
@@ -11,16 +27,39 @@ T = TypeVar('T')
 OneOrSeveral = Union[T, Sequence[T]]
 """A single scalar value or a sequence (list, tuple, ...) of scalar values"""
 
-BoundType = (
-    Literal['zero', 'replicate', 'dct1', 'dct2', 'dst1', 'dst2', 'dft']
-    if Literal else str)
-"""A Bound can be one of
-`{'zero', 'replicate', 'dct1', 'dct2', 'dst1', 'dst2', 'dft'}`"""
+BoundType = LiteralStr[
+    'zero', 'zeros',
+    'replicate', 'border', 'nearest', 'repeat',
+    'dct1', 'mirror',
+    'dct2', 'reflect',
+    'dst1', 'antimirror',
+    'dst2', 'antireflect',
+    'dft', 'wrap', 'circular', 'circulant',
+]
+"""
+There is a lack of standardization for bound names across packages.
+We mostly work with scipy's convention and/or with aliases that relate
+to the implicit boundary conditions in discrete transforms (Fourier, sine
+and cosine). We also accept less common aliases, although their use is
+discouraged
 
-OrderType = Literal[0, 1, 2, 3, 4, 5, 6, 7] if Literal else int
+| Fourier       | SciPy            | Other                                  | Description               |
+| ------------- | ---------------- | -------------------------------------- | ------------------------- |
+|               | `"border"`       | `"nearest"`, `"replicate"`, `"repeat"` | ` a  a | a b c d |  d  d` |
+|               | ~~`"constant"`~~ | `"zero"`, `"zeros"`                    | ` 0  0 | a b c d |  0  0` |
+| `"dct2"`      | `"reflect"`      |                                        | ` b  a | a b c d |  d  c` |
+| `"dct1"`      | `"mirror"`       |                                        | ` c  b | a b c d |  c  b` |
+| `"dft"`       | `"wrap"`         | `"circular"`, `"circulant"`            | ` c  d | a b c d |  a  b` |
+| `"dst2"`      |                  | `"antireflect"`                        | `-b -a | a b c d | -d -c` |
+| `"dst1"`      |                  | `"antimirror"`                         | `-a  0 | a b c d |  0 -d` |
+
+"""  # noqa: E501
+
+OrderType = LiteralInt[0, 1, 2, 3, 4, 5, 6, 7]
 """Interpolation orders are integers in the range `{0..7}`"""
 
-ExtrapolateType = Union[bool, Literal['center', 'edge'] if Literal else str]
+_ExtrapolateType = LiteralStr['center', 'edge']
+ExtrapolateType = Union[bool, _ExtrapolateType]
 """Extrapolation can be a boolean or one of `{'center', 'edge'}`
 
 - `True`: use bound to extrapolate out-of-bound value
@@ -30,7 +69,7 @@ of the centers of the first and last voxels.
 of the edges of the first and last voxels.
 """
 
-AnchorType = Literal['center', 'edge', None] if Literal else Optional[str]
+AnchorType = LiteralStrOpt['center', 'edge', None]
 """
 What feature should be aligned across the input and output tensors.
 
